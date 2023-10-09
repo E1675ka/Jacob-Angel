7f8f11f0b5fce1ae793d5b36bc91b37ab44f6098
E1675ka <jacobangel254@gmail.com>
2023-10-09 13:31:18 +0300

    Linked the contact form with gmail and fixed the paths

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// template_jicbddf
+// service_asu9acr
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -19,7 +21,7 @@ const Contact = () => {
 
   const handleChange = (e) => {
     const { target } = e;
-    const { name, value } = target;
+    const { name, value } = e.target;
 
     setForm({
       ...form,
@@ -33,8 +35,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_asu9acr",
+        "template_jicbddf",
         {
           from_name: form.name,
           to_name: "Jacob Angel",
@@ -42,7 +44,7 @@ const Contact = () => {
           to_email: "jacobangel254@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "AMhwWPKBFz_u1IlYy"
       )
       .then(
         () => {
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/ja.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
@@ -22,7 +22,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 7}
-        position={isMobile ? [0, -3, -2.2] : [0, -1.25, -1.5]}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
